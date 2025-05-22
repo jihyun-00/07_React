7404bb30002c18b9aae89799f05e0f12c63e0a41
jihyun-00 <rkdwl811@naver.com>
2025-05-22 10:58:40 +0900

    서버 연결

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -7,6 +7,8 @@ import Exam2 from "./components/Exam2";
 import Exam3 from "./components/Exam3";
 import Exam4 from "./components/Exam4";
 import Exam5 from "./components/Exam5";
+import Exam6 from "./components/Exam6";
+import Exam7 from "./components/Exam7";
 
 function App() {
   // 상태(state)
@@ -29,7 +31,7 @@ function App() {
     // </>
     // <Exam3 />
     // <Exam4 />
-    <Exam5 />
+    <Exam7 />
   );
 }
 
